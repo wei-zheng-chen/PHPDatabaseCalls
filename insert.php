189ab88e7d56cabca38277fc87cf4d4598b291c2
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-10-29 20:10:25 -0400

    tried insert

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+
+// Create connection
+$con=mysqli_connect("localhost","root","bitnami","health_alertdb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql = "INSERT INTO doctors (doctor_id, doctorName, doctorPhoneNumber, doctorAddress) 
+		VALUES (".$_GET['doctor_id'].", ".$_GET['doctorName'].", ".$_GET['doctorPhoneNumber'].", ".$_GET['doctorAddress'].")";
+
+if ($con->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+mysqli_close($con);
+
+?>
