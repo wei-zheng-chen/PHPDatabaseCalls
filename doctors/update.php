2382d0b17bbbd1f9210eec22b09d90421de1e13b
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-10-29 20:59:24 -0400

    fixing

--- doctors/update.php
+++ doctors/update.php
@@ -9,14 +9,14 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$doctor_id = "doctor_id"
-$doctorName = "doctorName"
-$doctorPhoneNumber = "doctorPhoneNumber"
-$doctorAddress = "doctorAddress"
+$doctor_id = "doctor_id";
+$doctorName = "doctorName";
+$doctorPhoneNumber = "doctorPhoneNumber";
+$doctorAddress = "doctorAddress";
 
-$map = [$_GET[$doctor_id], $_GET[$doctorName], $_GET[$doctorPhoneNumber], $_GET[$doctorAddress]]
+$map = [$_GET[$doctor_id], $_GET[$doctorName], $_GET[$doctorPhoneNumber], $_GET[$doctorAddress]];
 
-print $map
+print $map;
 
 
 
