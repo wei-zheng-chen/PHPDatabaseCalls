e1e3eb52fa5cd217b436959d4967c439e272451c
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-02 13:28:45 -0500

    patient delete added

---
+++ patients/delete.php
@@ -0,0 +1,29 @@
+<?php
+
+// Create connection
+$con = mysqli_connect("localhost", "root", "bitnami", "health_alertdb");
+
+if(mysqli_connect_errno()){
+	echo "Failed to connect to MySQL: ". mysqli_connect_errno();
+}
+
+//note deleting a doctor also requires a delete from the patient_doctor table
+
+$sql = "DELETE FROM patients WHERE patient_id = ".$_GET['patient_id']; 
+
+if($con->query($sql) == TRUE){
+	echo "deletion of a patient is successful\n";
+	// update patient_doctor table to relect the change
+	$sql = "DELETE FROM patients_doctors WHERE patient_id = ".$_GET['patient_id'];
+	if($con->query($sql) == TRUE){
+		echo "deletion from patients_doctors is successful";
+	}else{
+			echo "Error: " . $sql . "<br>" . $con->error;
+	}
+
+}else{
+	echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+mysqli_close($con);
+?>
