771c907194e9ad5b0de1595ac61c69ccc06de570
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-02 13:48:57 -0500

    modifiy patient select, added patient update

---
+++ patients/update.php
@@ -0,0 +1,25 @@
+<?php
+
+// Create connection
+$con=mysqli_connect("localhost","root","bitnami","health_alertdb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+$sql = "UPDATE patients
+		SET patientName = ".$_GET['patientName'];
+
+
+if ($con->query($sql) == TRUE) {
+    echo "updated record successfully\n";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+mysqli_close($con);
+
+?>
