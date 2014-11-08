c481535e26978c01cd57f549a6bc1d66a8596caf
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-08 10:45:34 -0500

    edited delete in contact to also edit the notifcation table

--- contacts/delete.php
+++ contacts/delete.php
@@ -13,14 +13,21 @@ $sql = "DELETE FROM patients_contacts WHERE contact_id = ".$_GET['contact_id'];
 
 if($con->query($sql) == TRUE){
 	echo "deletion of a patients_contacts is successful\n";
-	// update patient_doctor table to relect the change
-	$sql = "DELETE FROM contacts WHERE contact_id = ".$_GET['contact_id']; 
+	//update notifications
+	$sql = "DELETE FROM notifications WHERE contact_id = ".$_GET['contact_id'];
 	if($con->query($sql) == TRUE){
-		echo "deletion from contacts is successful";
+		echo "deletion of a notifications is successful\n";
+
+		// update patient_doctor table to relect the change
+		$sql = "DELETE FROM contacts WHERE contact_id = ".$_GET['contact_id']; 
+		if($con->query($sql) == TRUE){
+			echo "deletion from contacts is successful";
+		}else{
+			echo "Error: " . $sql . "<br>" . $con->error;
+		}
 	}else{
 			echo "Error: " . $sql . "<br>" . $con->error;
-	}
-
+		}
 }else{
 	echo "Error: " . $sql . "<br>" . $con->error;
 }
