be6e6ed474daa6bb98dc1ecd336cbb1ac0dbe0cd
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-08 10:36:46 -0500

    modified notification delete

--- notifications/delete.php
+++ notifications/delete.php
@@ -8,19 +8,32 @@ if(mysqli_connect_errno()){
 }
 
 //note deleting a doctor also requires a delete from the patient_doctor table
+$sql = ""
 
-$sql = "DELETE FROM notifications WHERE patient_id = ".$_GET['patient_id']; 
+if($_GET['patient_id']){
 
-if($_GET['recipientPhoneNumber']){
-	$sql = $sql . " AND recipientPhoneNumber = " .$_GET['recipientPhoneNumber'];
+	$sql = "DELETE FROM notifications WHERE patient_id = ".$_GET['patient_id']; 
+}
+
+if($_GET['patient_id'] && $_GET['stat_id']){
+	$sql = "DELETE FROM notifications WHERE patient_id = ".$_GET['patient_id'] . " AND 
+											stat_id = ". $_GET['stat_id']; 
 }
 
 
-if($con->query($sql) == TRUE){
-	echo "deletion of a notifications is successful\n";
+if($_GET['patient_id'] && $_GET['stat_id'] && $_GET['contact_id']){
 
-}else{
-	echo "Error: " . $sql . "<br>" . $con->error;
+$sql = "DELETE FROM notifications WHERE patient_id = ".$_GET['patient_id'] . " AND 
+											stat_id = ". $_GET['stat_id'] . " AND
+											contact_id = " . $_GET['contact_id']; 
+}
+if($sql != ""){	
+	if($con->query($sql) == TRUE){
+		echo "deletion of a notifications is successful\n";
+
+	}else{
+		echo "Error: " . $sql . "<br>" . $con->error;
+	}
 }
 
 mysqli_close($con);
