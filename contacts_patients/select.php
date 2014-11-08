7be7b89cd5f2052824d537320d2eb5a58241c5a4
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-08 08:46:43 -0500

    added select for patient_contact

---
+++ contacts_patients/select.php
@@ -0,0 +1,46 @@
+<?php
+ 
+// Create connection
+$con=mysqli_connect("localhost","root","bitnami","health_alertdb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql = "SELECT ".$_GET['attribute'] ." 
+        FROM patients_contacts";
+
+if($_GET['patient_id']){
+    $sql = $sql . " WHERE patient_id = ".$_GET['patient_id'];
+}
+
+if($_GET['contact_id']){
+    $sql = $sql . " WHERE contact_id = ".$_GET['contact_id'];
+}
+
+// Check if there are results
+if ($result = mysqli_query($con, $sql))
+{
+    // If so, then create a results array and a temporary one
+    // to hold the data
+    $resultArray = array();
+    $tempArray = array();
+
+    // Loop through each row in the result set
+    while($row = $result->fetch_object())
+    {
+        // Add each row into our results array
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+    }
+
+    // Finally, encode the array to JSON and output the results
+    echo json_encode($resultArray);
+}
+
+// Close connections
+mysqli_close($result);
+mysqli_close($con);
+?>
