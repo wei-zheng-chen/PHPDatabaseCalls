829306d983a163f26b7c59e2010515595c5a6576
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-08 10:58:31 -0500

    updated select to retrieve contact

--- notifications/select.php
+++ notifications/select.php
@@ -33,6 +33,13 @@ if($_GET['patient_id'] && $_GET['contact_id'] && $_GET['stat_id']){
         stat_id = ". $_GET['stat_id'];
 }
 
+if($_GET['patient_id'] && $_GET['stat_id']){
+    $sql = "SELECT ".$_GET['attribute'] ." 
+        FROM notifications
+        WHERE patient_id = ".$_GET['patient_id'] . " AND
+        stat_id = ". $_GET['stat_id'];
+}
+
 
 // Check if there are results
 if ($result = mysqli_query($con, $sql))
