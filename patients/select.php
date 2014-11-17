9fb26a181cfc249915bc3e99b7b296722f041a44
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-17 17:20:09 -0500

    added error

--- patients/select.php
+++ patients/select.php
@@ -34,7 +34,7 @@ if ($result = mysqli_query($con, $sql))
 
     // Finally, encode the array to JSON and output the results
     if(empty($resultArray)){
-        die('Invalid query, Query is empty');
+        http_response_code(404);
     }else{
         echo json_encode($resultArray);
     }
