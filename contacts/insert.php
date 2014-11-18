c69ebfca40f3392b52565342b976c29ed3ae0de8
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-18 07:42:49 -0500

    made changes to contact insert that also updates notificaitons

--- contacts/insert.php
+++ contacts/insert.php
@@ -12,7 +12,35 @@ if (mysqli_connect_errno())
 //NOTE: write a check to see if the entry is already in the database or not
 //Re-discuss if doctor_id should be autoincremented or inputted
 //Discuss if doctorPhoneNumber is Unique
+if($_GET['repeat'] !=""){
+    // get the contact id
+    $sql = "SELECT contact_id
+            FROM contacts
+            WHERE contactName =".$_GET['contactName']. "AND 
+            WHERE contactPhoneNumber =".$_GET['contactPhoneNumber'];
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0) {
+    // output data of each row
+        $contactID = "";
+        while($row = $result->fetch_assoc()) {
+            $contactID = $row["contact_id"];
+        }
+        //updating notifications
+        $sqlUpdate = "INSERT INTO notifications(patient_id,contact_id,stat_id)
+                        VALUES(".$patientID.", ". $contactID .", ".$_GET['stat_id'].")";
+        if($con->query($sqlUpdate)){
+            echo "New record in notifications created successfully\n";
+        }else{
+            echo "Error: " . $sql . "<br>" . $con->error ."\n";
+        }
+
+
+    } else {
+        echo "contact_id does not exist";
+    }
+
+}else{
 
 //insert a doct into table
 $sql = "INSERT INTO contacts (contactName, contactPhoneNumber) 
@@ -20,32 +48,41 @@ $sql = "INSERT INTO contacts (contactName, contactPhoneNumber)
 
 if ($con->query($sql) == TRUE) {
 
-    echo "New record created in contacts successfully\n";
-	$patientID = $_GET['patient_id'];
+        echo "New record created in contacts successfully\n";
+	   $patientID = $_GET['patient_id'];
 //updating patients_doctors table
-	if($patientID != ""){
-		$sqlUpdate = "INSERT INTO patients_contacts (patient_id, contact_id)
-			  		  VALUES(".$patientID.", ". $con->insert_id .")";
-		if ($con->query($sqlUpdate)) {
-   	 		echo "New record in patients_contacts created successfully";
-		} else {
-    		echo "Error: " . $sql . "<br>" . $con->error ."\n";
+	   if($patientID != ""){
+		  $sqlUpdate = "INSERT INTO patients_contacts (patient_id, contact_id)
+		      	  		  VALUES(".$patientID.", ". $con->insert_id .")";
+		  if ($con->query($sqlUpdate)) {
+   	 	      	echo "New record in patients_contacts created successfully\n";
+                //updating notifications
+                $sqlUpdate = "INSERT INTO notifications(patient_id,contact_id,stat_id)
+                               VALUES(".$patientID.", ". $con->insert_id .", ".$_GET['stat_id'].")";
+                if($con->query($sqlUpdate)){
+                    echo "New record in notifications created successfully\n";
+                }else{
+                    echo "Error: " . $sql . "<br>" . $con->error ."\n";
+                }
+
+		  } else {
+    		  echo "Error: " . $sql . "<br>" . $con->error ."\n";
     		
-    		$sqlUpdate = "DELETE FROM contacts 
+    		  $sqlUpdate = "DELETE FROM contacts 
     					  WHERE contact_id =" .$con->insert_id;
-    		if($con->query($sqlUpdate)){
-    			echo "The recently inserted contacts has been removed from the database since patients did not exist";
-    		}
-		}
-	}
+    		  if($con->query($sqlUpdate)){
+    			 echo "The recently inserted contacts has been removed from the database since patients did not exist";
+    		  }
+		  }
+	   }
 
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
+    } else {
+         echo "Error: " . $sql . "<br>" . $con->error;
 
-}
+    }
 //end of insert doc
 
-
+}
 
 
 mysqli_close($con);
