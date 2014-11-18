6379f123511b68bbfd809b96f3ab0f7b0e842239
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-17 19:07:44 -0500

    added update call to insert

--- bounds/insert.php
+++ bounds/insert.php
@@ -9,14 +9,46 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql = "INSERT INTO bounds (patient_id, stat_id, statLowerBound, statUpperBound) 
+//check if it in database
+$sql = "SELECT *
+        FROM bounds 
+        WHERE patient_id = ".$_GET['patient_id'];
+
+if($_GET['stat_id']){
+    $sql = $sql ." AND stat_id = ". $_GET['stat_id'];
+}
+
+
+// Check if there are results
+if ($result = mysqli_query($con, $sql)){
+	//if there is a result - update call
+	$sql = "UPDATE bounds
+		SET statLowerBound = ".$_GET['statLowerBound'].",
+			statUpperBound = ".$_GET['statUpperBound']."
+		WHERE patient_id = ".$_GET['patient_id']."
+		AND stat_id = ".$_GET['stat_id'];
+
+	if ($con->query($sql)){
+		echo "Updated record succesfully\n";
+	}else{
+		echo "Error: " . $sql . "<br>" . $con->error;
+
+	}
+
+}else{
+	//insert call
+	$sqlInsert = "INSERT INTO bounds (patient_id, stat_id, statLowerBound, statUpperBound) 
 		VALUES (".$_GET['patient_id'].", ".$_GET['stat_id'].", ".$_GET['statLowerBound'].", ".$_GET['statUpperBound'].")";
 
-if ($con->query($sql) == TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
+	if ($con->query($sqlInsert) == TRUE) {
+    	echo "New record created successfully";
+	} else {
+   		echo "Error: " . $sqlInsert . "<br>" . $con->error;
+	}
+
 }
+
+
 
 mysqli_close($con);
 
