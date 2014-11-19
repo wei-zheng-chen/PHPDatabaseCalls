db61c7f39fd20328c8657d64748a35d41d79667c
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-19 16:07:40 -0500

    edited patient select

--- patients/select.php
+++ patients/select.php
@@ -34,7 +34,48 @@ if ($result = mysqli_query($con, $sql))
 
     // Finally, encode the array to JSON and output the results
     if(empty($resultArray)){
-       echo http_response_code(404);
+
+        //added the unknow patient id
+        $sql = "INSERT INTO patients(patient_id, patientName) 
+                VALUES (".$_GET['patient_id'].", [unknown] )";
+
+        if ($con->query($sql) == TRUE) {
+         echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $con->error;
+        }
+
+        //retrive the unknown patient id again
+        $sql = "SELECT ".$_GET['attribute'] ." 
+        FROM patients";
+
+        if($_GET['patient_id']){
+            $sql = $sql . " WHERE patient_id = ".$_GET['patient_id'];
+        }
+
+        // Check if there are results
+        if ($result = mysqli_query($con, $sql)){
+             // If so, then create a results array and a temporary one
+            // to hold the data
+            $resultArray = array();
+            $tempArray = array();
+
+            // Loop through each row in the result set
+            while($row = $result->fetch_object())
+            {
+        // Add each row into our results array
+                $tempArray = $row;
+                array_push($resultArray, $tempArray);
+            }
+
+        // Finally, encode the array to JSON and output the results
+            if(empty($resultArray)){
+                echo "something has gone wrong, cant retrive the patient id". $_GET['patient_id'];
+            }
+        }else{
+                echo json_encode($resultArray);
+            }
+
     }else{
         echo json_encode($resultArray);
     }
