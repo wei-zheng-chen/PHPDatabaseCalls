fbfade16978cd43695aaa594bb03d111221ff3e8
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-19 23:13:18 -0500

    redo insert to take care of manual insert of callson and text on

--- notifications/insert.php
+++ notifications/insert.php
@@ -9,9 +9,16 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+if($_GET['textsOn'] && $_GET['callsOn']){
+
+$sql = "INSERT INTO notifications (patient_id, contact_id, stat_id, callsOn, textsOn) 
+		VALUES (".$_GET['patient_id'].", ".$_GET['contact_id'].", ".$_GET['stat_id'].", ".$_GET['callsOn']. ", " $_GET['textsOn'].")";
+
+}else{
+
 $sql = "INSERT INTO notifications (patient_id, contact_id, stat_id) 
 		VALUES (".$_GET['patient_id'].", ".$_GET['contact_id'].", ".$_GET['stat_id'].")";
-
+}
 if ($con->query($sql) == TRUE) {
     echo "New record created successfully";
 } else {
