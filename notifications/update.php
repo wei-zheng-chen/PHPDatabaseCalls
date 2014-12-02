67ef373eff04f9d929ea0a5a9676b079744c4b33
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-12-01 19:20:46 -0500

    added generic update to notifications

---
+++ notifications/update.php
@@ -0,0 +1,29 @@
+<?php
+
+// Create connection
+$con=mysqli_connect("localhost","root","bitnami","health_alertdb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+$sql = "UPDATE notifications
+		SET callsOn = ".$_GET['callsOn']." 
+		SET textsOn = ".$_GET['textsOn']."
+		WHERE patient_id = ".$_GET['patient_id']. 
+		"AND contact_id = ".$_GET['contact_id'].
+		"AND stat_id = " .$_GET['stat_id'] ;
+
+
+if ($con->query($sql) == TRUE) {
+    echo "updated record successfully\n";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+mysqli_close($con);
+
+?>
