5a146240745fd9c36e3cc08a794d45e9d4743a5c
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-12-01 21:57:44 -0500

    edit sql syntax

--- notifications/getAllTextsOn.php
+++ notifications/getAllTextsOn.php
@@ -9,9 +9,11 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql = "SELECT contact_id
-        FROM notifications
-        WHERE patient_id = ".$_GET['patient_id']." AND stat_id =".$_GET['stat_id']." AND textsOn = '1'";
+$sql = "SELECT * 
+        FROM contacts
+        WHERE contact_id IN (SELECT contact_id
+                             FROM notifications
+                             WHERE patient_id = ".$_GET['patient_id']." AND stat_id =".$_GET['stat_id']." AND textsOn = '1')";
 
 
 // Check if there are results
@@ -26,35 +28,13 @@ if ($result = mysqli_query($con, $sql))
     while($row = $result->fetch_object())
     {
         // Add each row into our results array
-        $tempArray = $row->contact_id;
+        $tempArray = $row;
         array_push($resultArray, $tempArray);
     }
 
-    var_dump($resultArray);
-    //get contact
-    $sql ="SELECT * 
-           FROM contacts
-           WHERE contact_id IN ".$resultArray ;
-
-    if ($result = mysqli_query($con, $sql)){
-    // If so, then create a results array and a temporary one
-    // to hold the data
-    $resultA = array();
-    $tempArray = array();
-
-    // Loop through each row in the result set
-    while($row = $result->fetch_object())
-    {
-        // Add each row into our results array
-        $tempArray = $row;
-        array_push($resultA, $tempArray);
-    }
-        echo json_encode($resultA);
-
-    }
 
     // Finally, encode the array to JSON and output the results
-    // echo json_encode($resultArray);
+    echo json_encode($resultArray);
 }
 
 // Close connections
