13eaf7842e1ea714a069b50a2a892d766b8c24af
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-12-01 21:53:12 -0500

    testing

--- notifications/getAllTextsOn.php
+++ notifications/getAllTextsOn.php
@@ -29,11 +29,29 @@ if ($result = mysqli_query($con, $sql))
         $tempArray = $row;
         array_push($resultArray, $tempArray);
     }
-    echo $resultArray
+
+    var_dump($resultArray);
     //get contact
     $sql ="SELECT * 
            FROM contacts
-           WHERE contact_id IN "
+           WHERE contact_id IN ".$resultArray ;
+
+    if ($result = mysqli_query($con, $sql)){
+    // If so, then create a results array and a temporary one
+    // to hold the data
+    $resultA = array();
+    $tempArray = array();
+
+    // Loop through each row in the result set
+    while($row = $result->fetch_object())
+    {
+        // Add each row into our results array
+        $tempArray = $row;
+        array_push($resultA, $tempArray);
+    }
+        echo json_encode($resultA);
+
+    }
 
     // Finally, encode the array to JSON and output the results
     // echo json_encode($resultArray);
